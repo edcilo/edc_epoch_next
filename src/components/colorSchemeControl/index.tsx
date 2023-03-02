6d9eef7c5ce3376a7e53338b7dee424e9038a0f4
edcilo <edcilop@gmail.com>
2023-03-01 21:33:53 -0600

    feat(color scheme): show shortcut depends of SO

--- src/components/colorSchemeControl/index.tsx
+++ src/components/colorSchemeControl/index.tsx
@@ -1,13 +1,15 @@
 import { ActionIcon, useMantineColorScheme, Tooltip } from "@mantine/core";
 import { IconSun, IconMoonStars } from "@tabler/icons-react";
 import { FC } from "react";
+import { useOs } from "@mantine/hooks";
 
 export const ColorSchemeControl: FC = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
+  const cmd = useOs() === "macos" ? "⌘" : "Ctrl";
 
   return (
-    <Tooltip label="Ctrl/⌘ + J">
+    <Tooltip label={`${cmd} + J`}>
       <ActionIcon
         variant="light"
         radius="xs"
