e000967139259f38fbbdedc032d597569cbdd821
edcilo <edcilop@gmail.com>
2023-03-07 13:49:06 -0600

    feat(404): add link go to home

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Link from "next/link";
 import { DefaultLayout } from "@/layouts";
 import { Stack, Container, Flex, Text, Title } from "@mantine/core";
 
@@ -13,6 +14,9 @@ const NotFoundPage: FC = () => {
             </Title>
             <Text align="center" size={32}>
               Page Not Found
+            </Text>
+            <Text align="center">
+              <Link href="/">Go to home page</Link>
             </Text>
           </Stack>
         </Flex>
