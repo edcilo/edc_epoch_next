3991a11a1986bef3acfc8d3a97ce66b56d6b3622
edcilo <edcilop@gmail.com>
2023-03-07 22:41:20 -0600

    fix(styles): horizontal scroll

--- src/components/background/styles.ts
+++ src/components/background/styles.ts
@@ -5,19 +5,19 @@ export const useStyles = createStyles((theme) => ({
   background: {
     position: "absolute",
     height: "70vh",
-    width: "100vw",
+    width: "100%",
   },
   backgroundImage: {
     position: "absolute",
     height: "70vh",
-    width: "100vw",
+    width: "100%",
     background:
       "url(https://storage.edcilo.com/images/mountain.webp) no-repeat center center",
   },
   backgroundColor: {
     position: "absolute",
     height: "70vh",
-    width: "100vw",
+    width: "100%",
     background: getColors(theme).primary,
     opacity: 0.8,
   },
@@ -25,6 +25,7 @@ export const useStyles = createStyles((theme) => ({
     position: "absolute",
     bottom: 0,
     left: 0,
+    right: 0,
   },
   wavePath: {
     fill: getColors(theme).background,
